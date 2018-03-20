5d9c134c2f140a006b306f33d908f4d21fdcc19d
Jeff Huang <herbage_h2h@sina.com>
2018-03-21 03:06:14 +0800

    add spring war

---
+++ hw3a-spring-war/src/main/java/cc/hw3a/spring/WarDemoApp.java
@@ -0,0 +1,12 @@
+package cc.hw3a.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarDemoApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarDemoApp.class, args);
+	}
+}
